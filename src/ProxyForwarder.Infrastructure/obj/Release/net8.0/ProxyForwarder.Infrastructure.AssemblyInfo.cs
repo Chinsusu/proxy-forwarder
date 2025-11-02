e7a24d139535b56abb73abc2ca5d2eb9d623fff5
Chinsu <lbthienan89@gmail.com>
2025-11-02 21:02:45 +0700

    fix: revert ImportViewModel to use working GetAllProxiesWithMetadataAsync endpoint

--- src/ProxyForwarder.Infrastructure/obj/Release/net8.0/ProxyForwarder.Infrastructure.AssemblyInfo.cs
+++ src/ProxyForwarder.Infrastructure/obj/Release/net8.0/ProxyForwarder.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProxyForwarder.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7a23cc6638a03a945146c724307032911e9800c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb1c4f0747256dcd5721837dc80728f97f85f1a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProxyForwarder.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProxyForwarder.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
